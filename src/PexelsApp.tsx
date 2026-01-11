9ca826963d930ba2f97f10e68e2b17da02dda4c1
RoockDev <zar0_31@outlook.com>
2026-01-11 01:06:16 +0100

    correcion env

--- src/PexelsApp.tsx
+++ src/PexelsApp.tsx
@@ -1,33 +1,30 @@
-import { ImageList } from './images/components/ImageList';
+import { ImageList } from "./images/components/ImageList";
 
-import { SearchBar } from './shared/components/SearchBar';
-import { CustomHeader } from './shared/components/CustomHeader';
+import { SearchBar } from "./shared/components/SearchBar";
+import { CustomHeader } from "./shared/components/CustomHeader";
 //import { PreviousSearches } from './images/components/PreviousSearches';
-import { getImagesByQuery } from './images/actions/get-images-by-query-action';
-import { useState } from 'react';
-import { type Image } from './images/interfaces/image.interface';
-
-
+import { getImagesByQuery } from "./images/actions/get-images-by-query-action";
+import { useState } from "react";
+import { type Image } from "./images/interfaces/image.interface";
 
 export const PexelsApp = () => {
+  const [images, setImages] = useState<Image[]>([]);
 
-  const [images,setImages] = useState<Image[]>([]);
-
-  const handleSearch = async (query:string) => {
+  const handleSearch = async (query: string) => {
     console.log(`Buscando: ${query}`);
 
     const newImages = await getImagesByQuery(query);
 
     setImages(newImages);
-  }
+  };
   return (
     <>
       <CustomHeader
-      title='Buscador de imagenes'
-      description='Busca tu imagen perfecta'
+        title="Buscador de imagenes"
+        description="Busca tu imagen perfecta"
       />
-      <SearchBar onNewSearch={handleSearch}/>
-      <ImageList images={images}/>
+      <SearchBar onNewSearch={handleSearch} />
+      <ImageList images={images} />
     </>
-  )
-}
+  );
+};
